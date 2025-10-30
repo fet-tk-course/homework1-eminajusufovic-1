582f354b956dc2f8c1cc49064c4b7c7efd29c013
Emina Jusufovic <emina.jusufovic@fet.ba>
2025-10-30 19:16:07 +0100

    Zadatak A – Emina Jusufović

--- HW1/A_eminajusufovic/src/main/kotlin/Main.kt
+++ HW1/A_eminajusufovic/src/main/kotlin/Main.kt
@@ -4,9 +4,33 @@ interface Person {
 }
 
 open class Developer (val firstName : String, val lastName : String, val yearsOfExp : Int,
-    val country : String, val lang : String) : Person {
+    val country : String, val lang : List<String>) : Person {
 
     override fun fullName() = "$firstName $lastName"
     override fun country() = country
 }
+class BackendDeveloper (firstName: String, lastName: String, yearsOfExp: Int,
+                        country: String, lang: List<String>, val backendFramework : String) : Developer(
+                            firstName, lastName, yearsOfExp, country, lang
+                        ){
+
+}
+
+class FrontendDeveloper (firstName: String, lastName: String, yearsOfExp: Int,
+                        country: String, lang: List<String>, val frontendFramework : String) : Developer(
+                            firstName, lastName, yearsOfExp, country, lang
+                        ){
+
+}
+
+fun countDeveloper(developers: List<Developer>): Map<String, Int> {
+    val allLanguages = developers.flatMap { dev -> dev.lang }
+    val languageGrouping = allLanguages.groupingBy { language -> language }
+    val languageCount = languageGrouping.eachCount()
+    return languageCount
+
+}
+
+
+
 
