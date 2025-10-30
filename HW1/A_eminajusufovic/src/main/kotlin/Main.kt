38e08fcd204e2fb99c350c129c9a7dd3fb623bb0
Emina Jusufovic <emina.jusufovic@fet.ba>
2025-10-30 19:36:15 +0100

    Zadatak A – Emina Jusufović

--- HW1/A_eminajusufovic/src/main/kotlin/Main.kt
+++ HW1/A_eminajusufovic/src/main/kotlin/Main.kt
@@ -31,6 +31,35 @@ fun countDeveloper(developers: List<Developer>): Map<String, Int> {
 
 }
 
+fun devInfo(dev : Developer){
+    val role : String
+    if (dev is BackendDeveloper){
+        role = "Backend developer"
+    }else if (dev is FrontendDeveloper){
+        role = "Frontend developer"
+    }else {
+        role = "N/A"
+    }
 
+
+}
+
+fun main (){
+    val developers = listOf(
+        BackendDeveloper("Emina", "Jusufovic", 5, "BA",listOf("Java", "Python"), "Ktor"),
+        FrontendDeveloper("Amila", "Residovic", 4, "US", listOf("JavaScript", "TypeScript"), "React"),
+        BackendDeveloper("Amina", "Hasic", 2, "UK",listOf("HTML", "CSS", "JavaScript"), "Vue.js" ),
+        FrontendDeveloper("Armin", "Coralic", 8, "BA", listOf("Java", "Kotlin"), "Ktor"),
+        BackendDeveloper("Adnan", "Hasic", 3, "DE", listOf("Python", "Java"), "Django"))
+
+    println("All developers")
+    developers.forEach { devInfo(it) }
+
+
+
+
+
+
+}
 
 
